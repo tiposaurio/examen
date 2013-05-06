9dc8f4592ef3ca068ae70fb78e431bc6b29abf46
tiposaurio <tiposurio@gmail.com>
2013-05-06 11:57:15 -0500

    mis archivos..

--- github.sh
+++ github.sh
@@ -1,7 +1,10 @@
+git init .
+git remote add origin git@hithub.com:tiposaurio/sistemasOperativos.git
 git add .
 git status
 git commit -a -m "mis archivos.."
 git status
+gitull origin master
 git push origin master
 git status
 ls
